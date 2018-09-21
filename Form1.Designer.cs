15b33dd3eec67c0da7b31e7fa626406a1193ac84
Amber Elferink <a.s.elferink@students.uu.nl>
2018-09-21 14:58:02 +0200

    Aangepaste layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,10 +61,9 @@
             // 
             // LoadImageButton
             // 
-            this.LoadImageButton.Location = new System.Drawing.Point(16, 15);
-            this.LoadImageButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LoadImageButton.Location = new System.Drawing.Point(12, 12);
             this.LoadImageButton.Name = "LoadImageButton";
-            this.LoadImageButton.Size = new System.Drawing.Size(131, 28);
+            this.LoadImageButton.Size = new System.Drawing.Size(98, 23);
             this.LoadImageButton.TabIndex = 0;
             this.LoadImageButton.Text = "Load image...";
             this.LoadImageButton.UseVisualStyleBackColor = true;
@@ -78,29 +77,26 @@
             // 
             // imageFileName
             // 
-            this.imageFileName.Location = new System.Drawing.Point(155, 17);
-            this.imageFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.imageFileName.Location = new System.Drawing.Point(116, 14);
             this.imageFileName.Name = "imageFileName";
             this.imageFileName.ReadOnly = true;
-            this.imageFileName.Size = new System.Drawing.Size(420, 22);
+            this.imageFileName.Size = new System.Drawing.Size(316, 20);
             this.imageFileName.TabIndex = 1;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(17, 55);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 45);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(579, 459);
+            this.pictureBox1.Size = new System.Drawing.Size(434, 373);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // applyButton
             // 
-            this.applyButton.Location = new System.Drawing.Point(1436, 14);
-            this.applyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.applyButton.Location = new System.Drawing.Point(1077, 11);
             this.applyButton.Name = "applyButton";
-            this.applyButton.Size = new System.Drawing.Size(280, 28);
+            this.applyButton.Size = new System.Drawing.Size(210, 23);
             this.applyButton.TabIndex = 3;
             this.applyButton.Text = "To grey-scale and auto contrast";
             this.applyButton.UseVisualStyleBackColor = true;
@@ -113,10 +109,9 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(1264, 14);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Location = new System.Drawing.Point(948, 11);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(127, 28);
+            this.saveButton.Size = new System.Drawing.Size(95, 23);
             this.saveButton.TabIndex = 4;
             this.saveButton.Text = "Save as BMP...";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -124,20 +119,18 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(708, 55);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(531, 45);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(525, 459);
+            this.pictureBox2.Size = new System.Drawing.Size(394, 373);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(783, 17);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Location = new System.Drawing.Point(587, 14);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(368, 25);
+            this.progressBar.Size = new System.Drawing.Size(276, 20);
             this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar.TabIndex = 6;
             this.progressBar.Visible = false;
@@ -146,10 +139,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1497, 75);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(945, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 17);
+            this.label1.Size = new System.Drawing.Size(214, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Kernel input. Seperation by space and enter";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -157,10 +149,9 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(1472, 222);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Location = new System.Drawing.Point(948, 188);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(104, 21);
+            this.radioButton1.Size = new System.Drawing.Size(79, 17);
             this.radioButton1.TabIndex = 9;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Linear Filter";
@@ -170,10 +161,9 @@
             // gaussianButton
             // 
             this.gaussianButton.AutoSize = true;
-            this.gaussianButton.Location = new System.Drawing.Point(1472, 250);
-            this.gaussianButton.Margin = new System.Windows.Forms.Padding(4);
+            this.gaussianButton.Location = new System.Drawing.Point(948, 211);
             this.gaussianButton.Name = "gaussianButton";
-            this.gaussianButton.Size = new System.Drawing.Size(118, 21);
+            this.gaussianButton.Size = new System.Drawing.Size(90, 17);
             this.gaussianButton.TabIndex = 10;
             this.gaussianButton.TabStop = true;
             this.gaussianButton.Text = "Gaussian Blur";
@@ -183,10 +173,9 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(1472, 278);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Location = new System.Drawing.Point(948, 234);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(110, 21);
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
             this.radioButton3.TabIndex = 11;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Median Filter";
@@ -196,10 +185,9 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(1472, 306);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton4.Location = new System.Drawing.Point(948, 257);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(89, 21);
+            this.radioButton4.Size = new System.Drawing.Size(70, 17);
             this.radioButton4.TabIndex = 12;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Max Filter";
@@ -208,10 +196,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1472, 371);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(948, 309);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 13;
             this.button1.Text = "Apply filter";
             this.button1.UseVisualStyleBackColor = true;
@@ -219,88 +206,88 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1501, 94);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(948, 84);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(213, 121);
+            this.textBox1.Size = new System.Drawing.Size(246, 99);
             this.textBox1.TabIndex = 7;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(1447, 609);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(945, 397);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(508, 22);
+            this.textBox2.Size = new System.Drawing.Size(382, 20);
             this.textBox2.TabIndex = 14;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1447, 586);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(945, 378);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 15;
             this.label2.Text = "Message";
             // 
             // gaussianInput
             // 
-            this.gaussianInput.Location = new System.Drawing.Point(1600, 250);
-            this.gaussianInput.Margin = new System.Windows.Forms.Padding(4);
+            this.gaussianInput.Location = new System.Drawing.Point(1044, 211);
             this.gaussianInput.Name = "gaussianInput";
             this.gaussianInput.ReadOnly = true;
-            this.gaussianInput.Size = new System.Drawing.Size(132, 22);
+            this.gaussianInput.Size = new System.Drawing.Size(100, 20);
             this.gaussianInput.TabIndex = 16;
             this.gaussianInput.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // medianSize
             // 
             this.medianSize.AutoSize = true;
-            this.medianSize.Location = new System.Drawing.Point(1597, 281);
+            this.medianSize.Location = new System.Drawing.Point(1042, 236);
+            this.medianSize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.medianSize.Name = "medianSize";
-            this.medianSize.Size = new System.Drawing.Size(33, 17);
+            this.medianSize.Size = new System.Drawing.Size(25, 13);
             this.medianSize.TabIndex = 19;
             this.medianSize.Text = "size";
             // 
             // medianSizeValue
             // 
-            this.medianSizeValue.Location = new System.Drawing.Point(1636, 277);
-            this.medianSizeValue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.medianSizeValue.Location = new System.Drawing.Point(1071, 233);
+            this.medianSizeValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.medianSizeValue.Name = "medianSizeValue";
             this.medianSizeValue.ReadOnly = true;
-            this.medianSizeValue.Size = new System.Drawing.Size(56, 22);
+            this.medianSizeValue.Size = new System.Drawing.Size(43, 20);
             this.medianSizeValue.TabIndex = 18;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1597, 306);
+            this.label3.Location = new System.Drawing.Point(1042, 257);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 21;
             this.label3.Text = "size";
             // 
             // maxFilterValue
             // 
-            this.maxFilterValue.Location = new System.Drawing.Point(1636, 305);
-            this.maxFilterValue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.maxFilterValue.Location = new System.Drawing.Point(1071, 256);
+            this.maxFilterValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.maxFilterValue.Name = "maxFilterValue";
             this.maxFilterValue.ReadOnly = true;
-            this.maxFilterValue.Size = new System.Drawing.Size(56, 22);
+            this.maxFilterValue.Size = new System.Drawing.Size(43, 20);
             this.maxFilterValue.TabIndex = 20;
             // 
             // thresholdTrackbar
             // 
             this.thresholdTrackbar.Enabled = false;
-            this.thresholdTrackbar.Location = new System.Drawing.Point(1606, 334);
+            this.thresholdTrackbar.Location = new System.Drawing.Point(1049, 279);
+            this.thresholdTrackbar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.thresholdTrackbar.Maximum = 255;
             this.thresholdTrackbar.Name = "thresholdTrackbar";
-            this.thresholdTrackbar.Size = new System.Drawing.Size(126, 56);
+            this.thresholdTrackbar.Size = new System.Drawing.Size(94, 45);
             this.thresholdTrackbar.TabIndex = 23;
             this.thresholdTrackbar.Value = 127;
             this.thresholdTrackbar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
@@ -308,9 +295,10 @@
             // thresholdValue
             // 
             this.thresholdValue.AutoSize = true;
-            this.thresholdValue.Location = new System.Drawing.Point(1738, 336);
+            this.thresholdValue.Location = new System.Drawing.Point(1148, 281);
+            this.thresholdValue.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.thresholdValue.Name = "thresholdValue";
-            this.thresholdValue.Size = new System.Drawing.Size(32, 17);
+            this.thresholdValue.Size = new System.Drawing.Size(25, 13);
             this.thresholdValue.TabIndex = 24;
             this.thresholdValue.Text = "127";
             this.thresholdValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -319,9 +307,10 @@
             // thresholdButton
             // 
             this.thresholdButton.AutoSize = true;
-            this.thresholdButton.Location = new System.Drawing.Point(1472, 334);
+            this.thresholdButton.Location = new System.Drawing.Point(948, 279);
+            this.thresholdButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.thresholdButton.Name = "thresholdButton";
-            this.thresholdButton.Size = new System.Drawing.Size(128, 21);
+            this.thresholdButton.Size = new System.Drawing.Size(97, 17);
             this.thresholdButton.TabIndex = 25;
             this.thresholdButton.TabStop = true;
             this.thresholdButton.Text = "Threshold Filter";
@@ -330,9 +319,9 @@
             // 
             // INFOIBV
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1815, 576);
+            this.ClientSize = new System.Drawing.Size(1374, 447);
             this.Controls.Add(this.thresholdButton);
             this.Controls.Add(this.thresholdValue);
             this.Controls.Add(this.thresholdTrackbar);
@@ -358,7 +347,6 @@
             this.Controls.Add(this.imageFileName);
             this.Controls.Add(this.LoadImageButton);
             this.Location = new System.Drawing.Point(10, 10);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "INFOIBV";
             this.ShowIcon = false;
             this.Text = "INFOIBV";
